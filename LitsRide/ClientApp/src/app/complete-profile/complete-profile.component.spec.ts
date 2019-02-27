e53eaf537d2776f857b29cd0ee478a5572ff82e7
Taha Abu Farweh <TahaF@sts.com.jo>
2019-02-28 00:24:57 +0200

    complete profile component added

---
+++ LitsRide/ClientApp/src/app/complete-profile/complete-profile.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { CompleteProfileComponent } from './complete-profile.component';
+
+let component: CompleteProfileComponent;
+let fixture: ComponentFixture<CompleteProfileComponent>;
+
+describe('complete-profile component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ CompleteProfileComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(CompleteProfileComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ LitsRide/ClientApp/src/app/complete-profile/complete-profile.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-complete-profile',
+    templateUrl: './complete-profile.component.html',
+    styleUrls: ['./complete-profile.component.scss']
+})
+/** complete-profile component*/
+export class CompleteProfileComponent {
+    /** complete-profile ctor */
+    constructor() {
+
+    }
+}
