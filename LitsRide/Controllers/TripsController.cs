40e4da9f9b1335ed3ef6b2c8112d251060019bc0
Hadeel AlDhmour <hadeeldm@sts.com.jo>
2019-03-08 19:53:50 +0200

    Hadeel's changes

--- LitsRide/Controllers/TripsController.cs
+++ LitsRide/Controllers/TripsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LitsRide.Models;
+using LitsRide.Models.Enum;
 
 namespace LitsRide.Controllers
 {
@@ -13,39 +14,27 @@ namespace LitsRide.Controllers
     [ApiController]
     public class TripsController : ControllerBase
     {
+        #region Variables
         private readonly LetsRideinContext _context;
+        #endregion
 
+        #region Constructor
         public TripsController(LetsRideinContext context)
         {
             _context = context;
         }
+        #endregion
 
-        // GET: api/Trips
-        [HttpGet]
-        [Route("GetAllTrips")]
-        public ActionResult GetTrip()
-        {
-            return Ok(_context.Trip.Include(x=>x.Driver).OrderByDescending(y => y.Id).ToList());
-        }
+        #region Public API
 
-
-        // GET: api/Trips/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Trip>> GetTrip(int id)
-        {
-            var trip = await _context.Trip.FindAsync(id);
-
-            if (trip == null)
-            {
-                return NotFound();
-            }
-
-            return trip;
-        }
-
-        // PUT: api/Trips/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTrip(int id, Trip trip)
+        /// <summary>
+        /// Create New Trip 
+        /// </summary>
+        /// <param name="NewTrip">Object of trip</param>
+        /// <returns>Trip </returns>
+        [HttpPost]
+        [Route("CreateNewTrip")]
+        public async Task<IActionResult> CreateNewTrip(int id, Trip trip)
         {
             if (id != trip.Id)
             {
@@ -56,6 +45,8 @@ namespace LitsRide.Controllers
 
             try
             {
+                trip.Status = (int)TripStatus.Opened;
+                _context.Trip.Add(trip);
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
@@ -73,23 +64,68 @@ namespace LitsRide.Controllers
             return NoContent();
         }
 
-        // POST: api/Trips
+        /// <summary>
+        /// Update Trip By ID 
+        /// </summary>
+        /// <param name="OldTrip">Object of trip</param>
+        /// <returns>Trip </returns>
+        /// 
         [HttpPost]
-        [Route("CreateNewTrip")]
-        public async Task<ActionResult<Trip>> PostTrip([FromBody]Trip trip)
+        [Route("UpdateTripByID")]
+        public async Task<IActionResult> UpdateTripByID(int id, Trip OldTrip)
         {
-
-            _context.Trip.Add(trip);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetTrip", new { id = trip.Id }, trip);
+            try
+            {
+                Trip NewTrip = _context.Trip.Where(trip => trip.Id == OldTrip.Id).FirstOrDefault();
+                NewTrip.FromDestination = OldTrip.FromDestination;
+                NewTrip.ToDestination = OldTrip.ToDestination;
+                NewTrip.StartTime = OldTrip.StartTime;
+                NewTrip.ArriveTime = OldTrip.ArriveTime;
+                NewTrip.DriverId = OldTrip.DriverId;
+                NewTrip.ExpectedArrivalTime = OldTrip.ExpectedArrivalTime;
+                NewTrip.Details = OldTrip.Details;
+                NewTrip.CarInfo = OldTrip.CarInfo;
+                NewTrip.Price = OldTrip.Price;
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TripExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
         }
 
-        // DELETE: api/Trips/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Trip>> DeleteTrip(int id)
+        /// <summary>
+        /// Update Trip Status 
+        /// </summary>
+        /// <param name="TripId">integer variable</param>
+        /// <param name="Status">String variable</param>
+        [HttpPost]
+        [Route("UpdateTripStatus")]
+        public async Task<IActionResult> UpdateTripStatus(int TripId, int Status)
         {
-            var trip = await _context.Trip.FindAsync(id);
+            Trip TripObj = _context.Trip.Where(trip => trip.Id == TripId).FirstOrDefault();
+            TripObj.Status = Status;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Delete trip 
+        /// </summary>
+        /// <param name="TripId">integer variable</param>
+        [HttpPost]
+        [Route("WithdrawTrip/{TripId}")]
+        public async Task<ActionResult<Trip>> WithdrawTrip(int TripId)
+        {
+            var trip = await _context.Trip.FindAsync(TripId);
             if (trip == null)
             {
                 return NotFound();
@@ -97,13 +133,96 @@ namespace LitsRide.Controllers
 
             _context.Trip.Remove(trip);
             await _context.SaveChangesAsync();
+            return trip;
+        }
+
+
+        /// <summary>
+        /// Get All Trips By Driver Id
+        /// </summary>
+        /// <param name="DriverId">integer variable</param>
+        /// <returns>list of trips </returns>
+        [HttpGet]
+        [Route("GetAllTripsByDriverId/{DriverId}")]
+        public async Task<ActionResult<IEnumerable<Trip>>> GetAllTripsByDriverId(int DriverId)
+        {
+            var trip = await _context.Trip.Where(x => x.DriverId == DriverId).ToListAsync();
+            if (trip == null)
+            {
+                return NotFound();
+            }
 
             return trip;
         }
 
+        /// <summary>
+        /// Get Trips Search
+        /// </summary>
+        /// <param name="Search">FilterTripsResource object</param>
+        /// <returns>list of trips </returns>
+        [HttpGet]
+        [Route("GetTripsSearch")]
+        public async Task<ActionResult<IEnumerable<Trip>>> GetTripsSearch(FilterTripsResource Search)
+        {
+            var trip = await _context.Trip.Where(x => (Search.StartTime == null || x.StartTime >= Search.StartTime)
+                                                    && (Search.PriceMin == null || x.Price >= Search.PriceMin)
+                                                    && (Search.PriceMax == null || x.Price <= Search.PriceMax)).ToListAsync();
+
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return trip;
+        }
+
+        /// <summary>
+        /// Get Trip By Trip Id 
+        /// </summary>
+        /// <param name="TripId">integer variable</param>
+        /// <returns>object of trips </returns>
+        [HttpGet]
+        [Route("GetTripByTripId")]
+        public async Task<ActionResult<Trip>> GetTripByTripId(int TripId)
+        {
+            var trip = await _context.Trip.Where(x => x.Id == TripId).FirstAsync();
+
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return trip;
+        }
+
+        /// <summary>
+        /// Check if trip startTime is passed 
+        /// </summary>
+        /// <param name="TripId">integer variable</param>
+        /// <returns>bool </returns> 
+        [HttpGet]
+        [Route("CheckIfTripPassed")]
+        public bool CheckIfTripPassed(int TripId)
+        {
+            return _context.Trip.Where(trip => trip.Id == TripId && trip.StartTime < DateTime.Now).ToList().Count() > 0 ? true : false;
+        }
+
+        /// <summary>
+        /// Check if trip startTime is passed 
+        /// </summary>
+        /// <param name="TripId">integer variable</param>
+        /// <returns>bool </returns> 
+        [HttpGet]
+        [Route("CheckIfTripPassed")]
+        public bool CheckIfTripFullBoard(int TripId)
+        {
+            return _context.Trip.Where(trip => trip.Id == TripId && trip.Status == (int)TripStatus.FullBoard).ToList().Count() > 0 ? true : false;
+        }
         private bool TripExists(int id)
         {
             return _context.Trip.Any(e => e.Id == id);
         }
+        #endregion
+
     }
 }
--- LitsRide/Models/Enum/Status.cs
+++ LitsRide/Models/Enum/Status.cs
@@ -5,11 +5,17 @@ using System.Threading.Tasks;
 
 namespace LitsRide.Models.Enum
 {
-    public class TripStatus
+    public enum TripStatus
     {
+        Opened = 1,
+        Closed = 2,
+        FullBoard = 3,
+        Deleted = 4
     }
 
-    public class TripRequestStatus
+    public enum TripRequestStatus
     {
+        Approved = 1,
+        Reject = 2
     }
 }
